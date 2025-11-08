707ac16f439af11ac8d13e46759397a42789e74c
300964582 <naamakisra@gmail.com>
2025-11-08 11:28:42 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -3,19 +3,19 @@ def move(my_list, direction):
 
     # if we want to move right
     if direction == "right":
-        # check if the pig is in the right edge- don't change location
+        # check if the pig is in the right edge - don't change location
         if index_of_one == len(my_list) - 1:
             return my_list
         # else move right
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
 
-# if we want to move left
+    # if we want to move left
     elif direction == "left":
-        # check if the pig is in the left edge- don't change location
+        # check if the pig is in the left edge - don't change location
         if index_of_one == 0:
             return my_list
-        #  else move left
+        # else move left
         my_list[index_of_one] = 0
         my_list[index_of_one - 1] = 1
 
